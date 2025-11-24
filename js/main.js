a06c65db878ab88a0301ec15c46aa8c7a196d508
Davydova Svetlana <svetach7777@bk.ru>
2025-11-24 21:05:56 +0300

    add: script for mobile-menu

--- js/main.js
+++ js/main.js
@@ -3,17 +3,37 @@ const logo = document.querySelector(".logo-svg use");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
+const lightModeOn = (event) => {
+  navbar.classList.add("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-dark";
+};
+
+const lightModeOff = (event) => {
+  navbar.classList.remove("navbar-light");
+  logo.href.baseVal = "img/sprite.svg#logo-light";
+};
+
+const openMenu = (event) => {
+  // функция открытия меню
+  menu.classList.add("isOpen"); // вешает класс isOpen
+  mMenuToggle.classList.add("close-menu");
+  document.body.style.overflow = "hidden"; // запрещаем прокрутку сайта под меню
+  lightModeOn();
+};
+
+const closeMenu = (event) => {
+  // функция закрытия меню
+  menu.classList.remove("isOpen"); // убирает класс isOpen
+  mMenuToggle.classList.remove("close-menu");
+  document.body.style.overflow = ""; // возвращаем прокрутку сайта под меню
+  lightModeOff();
+};
+
 window.addEventListener("scroll", () => {
-  if (this.scrollY > 1) {
-    navbar.classList.add("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-dark";
-  } else {
-    navbar.classList.remove("navbar-light");
-    logo.href.baseVal = "img/sprite.svg#logo-light";
-  }
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
 });
 
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
-  menu.classList.toggle("isOpen");
+  menu.classList.contains("isOpen") ? closeMenu() : openMenu();
 });
