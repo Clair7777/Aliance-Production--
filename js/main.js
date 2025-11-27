d92c7cfc431b2887849d9f0931beb636529f3b76
Davydova Svetlana <svetach7777@bk.ru>
2025-11-27 13:41:15 +0300

    fix: logo switcher

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 const navbar = document.querySelector(".navbar");
-const logo = document.querySelector(".logo-svg use");
+const logoLight = document.querySelector(".logo-light");
+const logo = document.querySelector(".logo");
 const mMenuToggle = document.querySelector(".mobile-menu-toggle");
 const menu = document.querySelector(".mobile-menu");
 
 const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
-  logo.href.baseVal = "img/sprite.svg#logo-dark";
+  logo.style.display = "block";
+  logoLight.style.display = "none";
+  // logo.href.baseVal = "img/sprite.svg#logo-dark";
 };
 
 const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light");
-  logo.href.baseVal = "img/sprite.svg#logo-light";
+  logo.style.display = "none";
+  logoLight.style.display = "block";
+  // logo.href.baseVal = "img/sprite.svg#logo-light";
 };
 
 const openMenu = (event) => {
